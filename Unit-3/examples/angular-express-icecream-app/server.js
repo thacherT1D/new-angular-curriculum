6a860ecd4f6dbdc8edd1bc1b63f42cf8e7d43ec3
Matt Lane <lane.matthew@gmail.com>
2015-11-30 09:38:55 -0800

    Update server.js

--- Unit-3/examples/angular-express-icecream-app/server.js
+++ Unit-3/examples/angular-express-icecream-app/server.js
@@ -10,14 +10,6 @@ app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
-// app.use(bodyParser.json());
-
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-//   next();
-// });
 
 app.get('/icecreams', function(req, res) {
   db.Icecream.find({},function(error,icecreams){
